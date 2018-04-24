291b52c903e8f97d334623b9f024c68675826aba
tutnatha <i.ketut.sunartha@gmail.com>
2018-04-24 09:34:54 +0700

    Update DaftarHunianAsramaController.java

--- src/main/java/com/course/springbootstarter/daftarhunian/asrama/DaftarHunianAsramaController.java
+++ src/main/java/com/course/springbootstarter/daftarhunian/asrama/DaftarHunianAsramaController.java
@@ -8,6 +8,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
+
 
 //import com.gigy.model.Party;
 //import com.gigy.repository.PartyRepository;
