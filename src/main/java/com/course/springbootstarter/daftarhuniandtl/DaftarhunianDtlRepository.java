80c80f95b1b371049ba8cac7d75bc4e5752f8889
tutnatha <i.ketut.sunartha@gmail.com>
2018-04-25 07:03:30 +0700

    Update DaftarhunianDtlRepository.java

--- src/main/java/com/course/springbootstarter/daftarhuniandtl/DaftarhunianDtlRepository.java
+++ src/main/java/com/course/springbootstarter/daftarhuniandtl/DaftarhunianDtlRepository.java
@@ -1,5 +1,6 @@
 package com.course.springbootstarter.daftarhuniandtl;
 
+import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 
 public interface DaftarhunianDtlRepository extends CrudRepository<DaftarhunianDtl, String> {
