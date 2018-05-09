8a3c1e130bb7ee6d2f3cc029a256d5b76de49661
tutnatha <i.ketut.sunartha@gmail.com>
2018-05-09 16:18:46 +0700

    commit from mcbook

--- src/main/java/com/course/springbootstarter/daftarhuniandtl/DaftarhunianDtl.java
+++ src/main/java/com/course/springbootstarter/daftarhuniandtl/DaftarhunianDtl.java
@@ -4,6 +4,7 @@
 
 package com.course.springbootstarter.daftarhuniandtl;
 
+import java.io.Serializable;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -11,6 +12,8 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.persistence.Column;
 import java.util.Date;
+import javax.persistence.Embeddable;
+import javax.persistence.EmbeddedId;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
 
@@ -22,18 +25,60 @@ import javax.persistence.NamedQuery;
 @NamedQuery(name="DaftarhunianDtl.findByNoTrx", 
         query="SELECT d FROM DaftarhunianDtl d WHERE d.no = ?1")
 })
-public class DaftarhunianDtl {
+public class DaftarhunianDtl implements Serializable{
 
     private static final long serialVersionUID = -3009157732242241606L;
-    @Id
+    
+    @EmbeddedId
+    private MyCompositePK id; //= new DaftarHunianAsramaDtl().new MyCompositePK();
+    
+//    @Id
 //    @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name="no_trx")
+//    @Column(name="no_trx")
     ///private int no;
-    private String no;          //catatan: pakai String dulu ya..
+//    private String no;          //catatan: pakai String dulu ya..
     @Column(name="seq_no")
     private int seqNo;
-    @Column(name="no_kamar")
-    private int noKamar;
+//    @Column(name="no_kamar")
+//    private int noKamar;
+
+        @Embeddable
+//    public class MyCompositePK implements Serializable{ 
+//
+//	However, I found that if I change class Id to static. Everything will be fine.
+//	I am just curious about how all these stuff can happen.
+//	https://stackoverflow.com/questions/6805143/no-default-constructor-for-entity-for-inner-class-in-hibernate
+//
+    //harus static : sumber stackoverflow.com/questions/6805143
+    public static class MyCompositePK implements Serializable{ 
+        @Column(name="no_trx")
+        private int noTrx;
+
+        @Column(name="no_kamar")
+        private String noKamar;
+        
+//        public MyCompositePK(){
+        protected MyCompositePK(){
+        	
+        }
+        
+    	public String getNoKamar() {
+    		return noKamar;
+    	}
+
+    	public void setNoKamar(String noKamar) {
+    		this.noKamar = noKamar;
+    	}
+
+        public int getNoTrx() {
+                return noTrx;
+        }
+
+        public void setNoTrx(int noTrx) {
+                this.noTrx = noTrx;
+        }
+
+    }
 
     public DaftarhunianDtl() {
 
@@ -42,22 +87,22 @@ public class DaftarhunianDtl {
     //    public DaftarhunianHdr(int no, String noKamar, String regNo, String isUsed, Date startDate, Date endDate) {
     public DaftarhunianDtl(String no, int seqNo, int noKamar) {
         super();
-        this.no = no;
+//        this.no = no;
         this.seqNo = seqNo;
-        this.noKamar = noKamar;
+//        this.noKamar = noKamar;
     }
 
     //    public int getNo() {
-    public String getNo() {
-        return no;
-    }
+//    public String getNo() {
+//        return no;
+//    }
 
     //    public void setNo(int no) {
 //        this.no = no;
 //    }
-    public void setNo(String no) {
-        this.no = no;
-    }
+//    public void setNo(String no) {
+//        this.no = no;
+//    }
 
     public int getSeqNo() {
         return seqNo;
@@ -67,12 +112,20 @@ public class DaftarhunianDtl {
         this.seqNo = seqNo;
     }
 
-    public int getNoKamar() {
-        return noKamar;
+//    public int getNoKamar() {
+//        return noKamar;
+//    }
+
+//    public void setNoKamar(int noKamar) {
+//        this.noKamar = noKamar;
+//    }
+
+    public MyCompositePK getId() {
+        return id;
     }
 
-    public void setNoKamar(int noKamar) {
-        this.noKamar = noKamar;
+    public void setId(MyCompositePK id) {
+        this.id = id;
     }
-
+    
 }
